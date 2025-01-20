9549171d6cd0c68b891d2cb8774a4285e3e38a7f
WhiredPlanck <whiredplanck@outlook.com>
2025-01-20 21:15:43 +0800

    Integrate lua addon

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -2,6 +2,7 @@ deps=(
   boost
   ecm
   fmt
+  lua
   libintl
   marisa
   opencc
@@ -21,6 +22,16 @@ done
 RES_EXTRACT_DIR=entry/src/main/resources/resfile/usr
 mkdir -p $RES_EXTRACT_DIR
 
-file=chinese-addons-any.tar.bz2
-[[ -f cache/$file ]] || wget -P cache https://github.com/fcitx-contrib/fcitx5-plugins/releases/download/macos/$file
-tar xf cache/$file -C $RES_EXTRACT_DIR lib share
+addons=(
+  chinese-addons
+  lua
+)
+for addon in "${addons[@]}"; do
+  file=$addon-any.tar.bz2
+  [[ -f cache/$file ]] || wget -P cache https://github.com/fcitx-contrib/fcitx5-plugins/releases/download/macos/$file
+  for dir in lib share; do
+    if tar tf "cache/$file" | grep -qE "^$dir($|/)"; then
+      tar xf cache/$file -C $RES_EXTRACT_DIR $dir
+    fi
+  done
+done
