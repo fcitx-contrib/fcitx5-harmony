dbdb783fb9f85194c700be040f743c8f169bc420
Qijia Liu <liumeo@pku.edu.cn>
2025-02-20 23:07:04 -0500

    set enter key type

--- entry/src/main/ets/InputMethodExtensionAbility/pages/keycode.ts
+++ entry/src/main/ets/InputMethodExtensionAbility/pages/keycode.ts
@@ -1,7 +1,8 @@
 import { KeyCode } from '@kit.InputKit'
 
 const jsCodeMap = {
-  Backspace: KeyCode.KEYCODE_DEL
+  Backspace: KeyCode.KEYCODE_DEL,
+  Enter: KeyCode.KEYCODE_ENTER,
 }
 
 export function convertCode(jsCode: string) {
