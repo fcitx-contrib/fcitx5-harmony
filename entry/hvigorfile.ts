1a3eaa28d10336826b4f9498a3b70716291204f8
Panda527 <whiredplanck@outlook.com>
2025-02-23 06:18:48 +0800

    dynamic set build abi filters (#18)

--- entry/hvigorfile.ts
+++ entry/hvigorfile.ts
@@ -1,4 +1,18 @@
-import { hapTasks } from '@ohos/hvigor-ohos-plugin';
+import { hapTasks, OhosHapContext, OhosPluginId } from '@ohos/hvigor-ohos-plugin'
+import { getNode, hvigor } from '@ohos/hvigor'
+
+const param = hvigor.getParameter()
+const buildAbiOverride = process.env['BUILD_ABI'] ?? param.getExtParam('buildABI')
+const supportedAbis = ['arm64-v8a', 'x86_64']
+
+const rootNode = getNode(__filename)
+rootNode.afterNodeEvaluate(node => {
+    const appContext = node.getContext(OhosPluginId.OHOS_HAP_PLUGIN) as OhosHapContext;
+    const buildProfileOpt = appContext.getBuildProfileOpt()
+    buildProfileOpt['buildOption']['externalNativeOptions']['abiFilters'] =
+        buildAbiOverride?.split(',') ?? supportedAbis
+    appContext.setBuildProfileOpt(buildProfileOpt)
+})
 
 export default {
     system: hapTasks,  /* Built-in plugin of Hvigor. It cannot be modified. */
