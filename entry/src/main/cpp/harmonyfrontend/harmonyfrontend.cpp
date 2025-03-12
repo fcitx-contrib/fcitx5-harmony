d112d96f8137e3193f992f791a1f81c86e49ee82
WhiredPlanck <whiredplanck@outlook.com>
2025-03-12 13:19:09 +0800

    commit and update preedit async

--- entry/src/main/cpp/harmonyfrontend/harmonyfrontend.cpp
+++ entry/src/main/cpp/harmonyfrontend/harmonyfrontend.cpp
@@ -1,4 +1,7 @@
 #include "harmonyfrontend.h"
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
 
 namespace fcitx {
 HarmonyFrontend::HarmonyFrontend(Instance *instance)
@@ -13,7 +16,9 @@ void HarmonyFrontend::createInputContext() {
 
 InputContextState HarmonyFrontend::keyEvent(const Key &key, bool isRelease) {
     KeyEvent event(ic_, key, isRelease);
+    ic_->isSyncEvent = true;
     ic_->keyEvent(event);
+    ic_->isSyncEvent = false;
     return ic_->popState(event.accepted());
 }
 
@@ -37,12 +42,20 @@ HarmonyInputContext::HarmonyInputContext(HarmonyFrontend *frontend, InputContext
 
 HarmonyInputContext::~HarmonyInputContext() { destroy(); }
 
-void HarmonyInputContext::commitStringImpl(const std::string &text) { state_.commit += text; }
+void HarmonyInputContext::commitStringImpl(const std::string &text) {
+    state_.commit += text;
+    if (!isSyncEvent) {
+        commitStringAsync();
+    }
+}
 
 void HarmonyInputContext::updatePreeditImpl() {
-    auto preedit = frontend_->instance()->outputFilter(this, inputPanel().clientPreedit());
+    auto preedit = filterText(inputPanel().clientPreedit());
     state_.preedit = preedit.toString();
     state_.cursorPos = preedit.cursor();
+    if (!isSyncEvent) {
+        updatePreeditAsync();
+    }
 }
 
 InputContextState HarmonyInputContext::popState(bool accepted) {
@@ -52,6 +65,20 @@ InputContextState HarmonyInputContext::popState(bool accepted) {
     state.accepted = accepted;
     return state;
 }
+
+void HarmonyInputContext::commitStringAsync() {
+    auto state = state_;
+    state_.commit = "";
+    auto j = json{{"type", "COMMIT"}, {"data", state.commit}};
+    notify_main_async(j.dump());
+}
+
+void HarmonyInputContext::updatePreeditAsync() {
+    auto state = state_;
+    auto j = json{{"type", "PREEDIT"}, {"data", {{"preedit", state.preedit}, {"cursorPos", state.cursorPos}}}};
+    notify_main_async(j.dump());
+}
+
 } // namespace fcitx
 
 FCITX_ADDON_FACTORY_V2(harmonyfrontend, fcitx::HarmonyFrontendFactory);
