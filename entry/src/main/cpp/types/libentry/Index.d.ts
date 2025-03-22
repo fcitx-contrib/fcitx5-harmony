426091bd6652e4bd587cf3bcc66f0781846ca17a
WhiredPlanck <whiredplanck@outlook.com>
2025-03-22 19:51:25 +0800

    use ArkTS as more as possible

--- entry/src/main/cpp/types/libentry/Index.d.ts
+++ entry/src/main/cpp/types/libentry/Index.d.ts
@@ -3,11 +3,13 @@ export const setCallback: (callback: (data: string) => void) => void
 export const focusIn: (clientPreedit: boolean) => void
 export const focusOut: () => void
 export const reset: () => void
-export const processKey: (unicode: number, keyCode: number, isRelease: boolean) => {
+
+export interface InputContextState {
   commit: string
   preedit: string
   cursorPos: number
   accepted: boolean
 }
+export const processKey: (unicode: number, keyCode: number, isRelease: boolean) => InputContextState
 export const selectCandidate: (index: number) => void
 export const activateStatusAreaAction: (index: number) => void
