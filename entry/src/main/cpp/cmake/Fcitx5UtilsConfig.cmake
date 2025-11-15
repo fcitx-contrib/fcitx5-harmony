1cf13f2d3af8efe91d5066cde7fb6d6cc538cab6
Qijia Liu <liumeo@pku.edu.cn>
2025-11-15 18:20:38 -0500

    revert set FCITX_INSTALL_PKGDATADIR

--- entry/src/main/cpp/cmake/Fcitx5UtilsConfig.cmake
+++ entry/src/main/cpp/cmake/Fcitx5UtilsConfig.cmake
@@ -8,5 +8,5 @@ set(FCITX_INSTALL_CMAKECONFIG_DIR "${CMAKE_CURRENT_LIST_DIR}")
 # mimic fcitx5/src/lib/fcitx-utils/Fcitx5UtilsConfig.cmake.in
 include("${CMAKE_CURRENT_LIST_DIR}/Fcitx5Utils/Fcitx5Macros.cmake")
 
-# Unify addons with fcitx5.
-set(FCITX_INSTALL_LOCALEDIR "/usr/local/share/locale")
+# Install resources to entry/src/main/resources/resfile/usr/share/fcitx5
+set(FCITX_INSTALL_PKGDATADIR /usr/share/fcitx5)
